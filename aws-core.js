b0d11d5564fa149b53757d67208738edcadecc5b
Mike Plavsky <Mike.Plavsky@quest.com>
2011-10-03 17:07:45 +0000

    refactoring

--- aws-core.js
+++ aws-core.js
@@ -1,11 +1,10 @@
 var aws = require ('aws-lib');
-var evs = require('events');
+var EventEmitter = require('events').EventEmitter;
 var _ = require('underscore');
 
 module.exports = function describe_instances(opts) {
 
-  var events = new evs.EventEmitter();
-
+  var events = new EventEmitter();
   var ec2 = aws.createEC2Client(opts.access_key_id, opts.secret_access_key, {version:'2011-07-15'});
 
   ec2.call( "DescribeInstances", 
